c682a9d28cc494798464ed63f555f26b24d7d2eb
mohamedctm <52657504+mohamedctm@users.noreply.github.com>
2025-02-04 09:31:39 -0500

    fixed by colpilot

--- stylelint.config.ts
+++ stylelint.config.ts
@@ -3,7 +3,7 @@ module.exports = {
     "at-rule-no-unknown": [
       true,
       {
-        ignoreAtRules: ["tailwind", "apply", "variants", "responsive", "screen"]
+        ignoreAtRules: ["apply", "variants", "responsive", "screen"]
       }
     ]
   }
